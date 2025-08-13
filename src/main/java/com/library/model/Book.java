7672bb8830129b3d203edb220309514888baac82
Rahul-hackz <rahulramados2004@gmail.com>
2025-08-13 12:27:47 +0530

    Update Book.java

--- src/main/java/com/library/model/Book.java
+++ src/main/java/com/library/model/Book.java
@@ -1,27 +1,42 @@
 package com.library.model;
 
 public class Book {
-    private int bookId;
+    private int id;
     private String title;
     private String author;
-    private String category;
-    private int quantity;
 
+    // Default constructor
     public Book() {}
 
-    public Book(int bookId, String title, String author, String category, int quantity) {
-        this.bookId = bookId; this.title = title; this.author = author; this.category = category; this.quantity = quantity;
+    // Constructor with fields
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
     }
 
-    // getters & setters
-    public int getBookId() { return bookId; }
-    public void setBookId(int bookId) { this.bookId = bookId; }
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-    public String getAuthor() { return author; }
-    public void setAuthor(String author) { this.author = author; }
-    public String getCategory() { return category; }
-    public void setCategory(String category) { this.category = category; }
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
